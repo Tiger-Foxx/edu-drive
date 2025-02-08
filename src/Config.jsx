1727612600c2ebd3c37789de5f82e8590f61a34b
Tiger-Foxx <donfackarthur750@gmail.com>
2025-02-08 13:35:33 +0100

    ajout du compte campay reel

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,8 +1,8 @@
 // export const SERVER_BASE_URL = 'http://192.168.100.208:8000';
 export const SERVER_BASE_URL = 'https://formatplus.pythonanywhere.com';
 
-export const YOUR_CAMPAY_API_TOKEN='b6dc738b72e316bdabef072ec9a977bacb24b239';
+export const YOUR_CAMPAY_API_TOKEN='926208355b0ed1a0c6ecfb68d5491a8dc5cf7f74';
 export const MONEY_FUSION_URL='https://www.pay.moneyfusion.net/Formatplus/42ba88d7da48a4ed/pay/';
 export  const PRICE_TELEGRAM_SUBSCRIPTION=5;
 export const PRICE_FORMAT_PLUS_SUBSCRIPTION=5;
-export const IS_DEMO=true;
+export const IS_DEMO=false;
