e445fe46fa02c7ae90244b36d27b628df8c2ebcb
Tiger-Foxx <donfackarthur750@gmail.com>
2025-02-08 12:56:35 +0100

    integration correcte de campay

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,5 +1,5 @@
-export const SERVER_BASE_URL = 'http://192.168.100.208:8000';
-// export const SERVER_BASE_URL = 'https://formatplus.pythonanywhere.com';
+// export const SERVER_BASE_URL = 'http://192.168.100.208:8000';
+export const SERVER_BASE_URL = 'https://formatplus.pythonanywhere.com';
 
 export const YOUR_CAMPAY_API_TOKEN='b6dc738b72e316bdabef072ec9a977bacb24b239';
 export const MONEY_FUSION_URL='https://www.pay.moneyfusion.net/Formatplus/42ba88d7da48a4ed/pay/';
