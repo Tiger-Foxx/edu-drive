c8c9e8037578480ee2a5b89e9b935cea0ef9ad67
Tiger-Foxx <donfackarthur750@gmail.com>
2025-02-09 18:14:35 +0100

    corrections et prix

--- src/Config.jsx
+++ src/Config.jsx
@@ -3,6 +3,6 @@ export const SERVER_BASE_URL = 'https://formatplus.pythonanywhere.com';
 
 export const YOUR_CAMPAY_API_TOKEN='cf9da2372690b199aa0edca0917419d729802dc6';
 export const MONEY_FUSION_URL='https://www.pay.moneyfusion.net/Formatplus/42ba88d7da48a4ed/pay/';
-export  const PRICE_TELEGRAM_SUBSCRIPTION=5;
-export const PRICE_FORMAT_PLUS_SUBSCRIPTION=5;
+export  const PRICE_TELEGRAM_SUBSCRIPTION=10000;
+export const PRICE_FORMAT_PLUS_SUBSCRIPTION=2900;
 export const IS_DEMO=false;
