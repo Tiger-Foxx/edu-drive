6d40d2a2e070734527a69b5c98d73ee8855c8d60
Tiger-Foxx <donfackarthur750@gmail.com>
2025-05-23 15:17:21 +0100

    avant modification de l'api de paiement

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,5 +1,5 @@
-// export const SERVER_BASE_URL = 'http://192.168.100.208:8000';
-export const SERVER_BASE_URL = 'https://formatplus.pythonanywhere.com';
+export const SERVER_BASE_URL = 'http://127.0.0.1:8000';
+// export const SERVER_BASE_URL = 'https://formatplus.pythonanywhere.com';
 
 export const YOUR_CAMPAY_API_TOKEN='cf9da2372690b199aa0edca0917419d729802dc6';
 export const MONEY_FUSION_URL='https://www.pay.moneyfusion.net/Formatplus/42ba88d7da48a4ed/pay/';
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// export const SERVER_BASE_URL = 'http://192.168.100.208:8000';
-export const SERVER_BASE_URL = 'https://formatplus.pythonanywhere.com';
+export const SERVER_BASE_URL = 'http://127.0.0.1:8000';
+// export const SERVER_BASE_URL = 'https://formatplus.pythonanywhere.com';
 
 
 
